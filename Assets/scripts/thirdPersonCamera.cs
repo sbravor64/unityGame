9d14302956db31940bba3dcbb3d57c4fbec3901b
Andres Bravo <sbravor64@elpuig.xeill.net>
2019-11-04 12:48:42 +0100

    mejorado

---
+++ Assets/scripts/thirdPersonCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdPersonCamera : MonoBehaviour
+{
+
+    public Vector3 offset;
+    private Transform target;
+    [Range(0,1)] public float lerpValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, lerpValue);
+    }
+} 
